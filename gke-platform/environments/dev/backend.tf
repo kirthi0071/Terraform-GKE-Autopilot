a55695c0c97a9dd77137588029640b975b421bf5
Kirthi Kumar <kirthikumar077@gmail.com>
2026-07-29 11:48:55 +0530

    Delete gke-platform directory

--- gke-platform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-# Backend values are intentionally left blank here because a `backend` block
-# cannot reference variables or .tfvars. Bucket/prefix are supplied at
-# `terraform init` time via -backend-config, using backend-dev.hcl below.
-#
-#   terraform init -backend-config=backend-dev.hcl
-#
-terraform {
-  backend "gcs" {}
-}
