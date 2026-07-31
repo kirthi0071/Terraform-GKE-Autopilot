98c4551971667f51923722586f46a4ae73c64513
Kirthi Kumar <kirthikumar077@gmail.com>
2026-07-31 21:30:47 +0530

    Delete gke-platform directory

--- gke-platform/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "service_account_email" {
-  description = "The email of the GKE service account"
-  value       = google_service_account.gke_sa.email
-}
-
-output "service_account_id" {
-  description = "Fully qualified ID of the GKE service account"
-  value       = google_service_account.gke_sa.id
-}
