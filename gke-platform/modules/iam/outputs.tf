f28294102747761a0b1b2cde19cdc4df42115a86
Kirthi Kumar <kirthikumar077@gmail.com>
2026-07-31 20:49:21 +0530

    Update outputs.tf

--- gke-platform/modules/iam/outputs.tf
+++ gke-platform/modules/iam/outputs.tf
@@ -1,14 +1,9 @@
 output "service_account_email" {
-  description = "Email of the created GKE service account"
+  description = "Email of the GKE node/workload service account, consumed by the gke/ root module via remote state"
   value       = google_service_account.gke_sa.email
 }
 
 output "service_account_id" {
-  description = "Fully qualified ID of the created GKE service account"
+  description = "Fully qualified ID of the GKE service account"
   value       = google_service_account.gke_sa.id
-}
-
-output "service_account_name" {
-  description = "Fully qualified resource name of the created GKE service account"
-  value       = google_service_account.gke_sa.name
 }
