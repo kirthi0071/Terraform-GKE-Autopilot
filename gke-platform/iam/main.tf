b3e32012533472f79a29f71d13201cfc2e7b919d
Kirthi Kumar <kirthikumar077@gmail.com>
2026-07-31 20:48:39 +0530

    Update main.tf

--- gke-platform/iam/main.tf
+++ gke-platform/iam/main.tf
@@ -1,9 +1,34 @@
-module "iam" {
-  source = "../modules/iam"
+# -----------------------------------------------------------------------------
+# Service account used by GKE Autopilot nodes / workloads
+# -----------------------------------------------------------------------------
+resource "google_service_account" "gke_sa" {
+  project      = var.project_id
+  account_id   = var.service_account_id
+  display_name = var.service_account_display_name
+}
 
-  project_id                    = var.project_id
-  service_account_id            = var.service_account_id
-  service_account_display_name  = var.service_account_display_name
-  project_roles                 = var.project_roles
-  bindings                      = var.sa_bindings
+# -----------------------------------------------------------------------------
+# Project-level roles
+# -----------------------------------------------------------------------------
+resource "google_project_iam_member" "gke_sa_roles" {
+  for_each = toset(var.project_roles)
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.gke_sa.email}"
+}
+
+# -----------------------------------------------------------------------------
+# IAM policy bindings ON the service account itself
+# -----------------------------------------------------------------------------
+module "service_accounts_iam" {
+  source  = "terraform-google-modules/iam/google//modules/service_accounts_iam"
+  version = "~> 8.0"
+
+  project          = var.project_id
+  service_accounts = [google_service_account.gke_sa.email]
+  mode             = var.mode
+  bindings         = var.bindings
+
+  depends_on = [google_service_account.gke_sa]
 }
